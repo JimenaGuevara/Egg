2befcd050558e4d6e80b2074ffb8258a905b62ab
Jimena Guevara <jimenaguevara28@gmail.com>
2021-10-13 17:30:30 -0300

    Segundo Commit

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplogit;
 
-/**
- *
- * @author Usuario
- */
 public class EjemploGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int num;
+        int num2;
     }
-    
 }
